02a0739bf286d5c2e2db0ad6d0d4f51f10f394c0
Jake Pan <jiaxing1222@gmail.com>
2022-06-18 11:10:08 +0800

    added sign in component

--- src/routes/home/Home.jsx
+++ src/routes/home/Home.jsx
@@ -1,8 +1,9 @@
 import { Routes, Route } from "react-router-dom"
 import React from "react"
 
-import DirectoryMenu from "../components/directory/DirectoryMenu"
-import Navigation from "../components/navigation/Navigation"
+import DirectoryMenu from "../../components/directory/DirectoryMenu"
+import Navigation from "../../components/navigation/Navigation"
+import SignIn from "../sign-in/SignIn"
 
 const Home = () => {
   return (
@@ -10,6 +11,7 @@ const Home = () => {
       <Routes>
         <Route path="/" element={<Navigation />}>
           <Route index element={<DirectoryMenu />} />
+          <Route path="sign-in" element={<SignIn />} />
         </Route>
       </Routes>
     </>
