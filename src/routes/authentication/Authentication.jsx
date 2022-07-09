13661829ce3718c63f74a076cce9ce0e891690d9
Jake Pan <jiaxing1222@gmail.com>
2022-07-09 16:08:07 +0800

    created styles for sign in page and renamed component

---
+++ src/routes/authentication/Authentication.jsx
@@ -0,0 +1,13 @@
+import SignUp from "../../components/sign-up-form/SignUp"
+import SignIn from "../../components/sign-in-form/SignIn"
+
+const Authentication = () => {
+  return (
+    <>
+      <SignIn />
+      <SignUp />
+    </>
+  )
+}
+
+export default Authentication
