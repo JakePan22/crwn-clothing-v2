b1668692fead8804438d6382bbd197d51338d92c
Jake Pan <jiaxing1222@gmail.com>
2022-07-25 19:11:17 +0800

    added sign out method

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -5,6 +5,7 @@ import {
   GoogleAuthProvider,
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
+  signOut,
 } from "firebase/auth"
 import { doc, getDoc, getFirestore, setDoc } from "firebase/firestore"
 
@@ -70,4 +71,8 @@ export const createAuthUserWithEmailAndPassword = async (email, password) => {
 
 export const signInAuthUserWithEmailAndPassword = async (email, password) => {
   return await signInWithEmailAndPassword(auth, email, password)
+}
+
+export const signOutUser = async () => {
+  await signOut(auth)
 }
