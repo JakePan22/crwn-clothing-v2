44698adcb04d0b5e17fde2957869751edbf5b8b0
Jake Pan <jiaxing1222@gmail.com>
2022-07-26 03:10:37 +0800

    renaming context and putting in types for createContext

--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -4,7 +4,10 @@ import {
   createUserDocumentFromAuth,
 } from "../utils/firebase/firebase.utils"
 
-export const UserContext = createContext()
+export const UserContext = createContext({
+  currentUser: null,
+  setCurrentUser: () => null,
+})
 
 export const UserProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState(null)
