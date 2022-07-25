a481954fca4e1d83a49091dda4031cb45069bebe
Jake Pan <jiaxing1222@gmail.com>
2022-07-25 19:46:33 +0800

    added in useEffect handling for listerner and setting state

--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -1,10 +1,24 @@
-import { useState, createContext } from "react"
+import { useState, createContext, useEffect } from "react"
+import {
+  onAuthStateChangedListener,
+  createUserDocumentFromAuth,
+} from "../utils/firebase/firebase.utils"
 
 export const UserContext = createContext()
 
 export const UserProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState(null)
   const value = { currentUser, setCurrentUser }
+
+  useEffect(() => {
+    onAuthStateChangedListener(async (user) => {
+      if (user) {
+        await createUserDocumentFromAuth(user)
+      }
+
+      setCurrentUser(user)
+    })
+  }, [])
 
   return <UserContext.Provider value={value}>{children}</UserContext.Provider>
 }
