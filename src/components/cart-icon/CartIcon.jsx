a62bb3d04580ebc07fa031c7f3bcbb8b9606d0a5
Jake Pan <jiaxing1222@gmail.com>
2022-07-27 23:15:55 +0800

    added onclick handler and toggle. gotcha is you cannot trigger onClick on your own components, you have to use the HTML tags within the components

--- src/components/cart-icon/CartIcon.jsx
+++ src/components/cart-icon/CartIcon.jsx
@@ -1,9 +1,17 @@
+import { useContext } from "react"
 import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg"
 import "./cart-icon.styles.scss"
 
+import { CartContext } from "../../contexts/CartContext"
+
 const CartIcon = () => {
+  const { isCartOpen, setIsCartOpen } = useContext(CartContext)
+
+  const toggleCartDropdown = () => {
+    setIsCartOpen(!isCartOpen)
+  }
   return (
-    <div className="cart-icon-container">
+    <div className="cart-icon-container" onClick={toggleCartDropdown}>
       <ShoppingIcon className="shopping-icon" />
       <span className="item-count">10</span>
     </div>
