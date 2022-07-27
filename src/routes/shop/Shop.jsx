f27b8f2d5e8d070b844c5b4b21dad795b32a96fe
Jake Pan <jiaxing1222@gmail.com>
2022-07-27 21:59:46 +0800

    used component

--- src/routes/shop/Shop.jsx
+++ src/routes/shop/Shop.jsx
@@ -1,19 +1,18 @@
 import { useContext } from "react"
 import { ProductsContext } from "../../contexts/ProductsContext"
+import ProductCard from "../../components/product-card/ProductCard"
+
+import "./shop-styles.scss"
 
 const Shop = ({}) => {
   const { products } = useContext(ProductsContext)
 
   return (
-    <>
-      {products.map(({ id, name }) => {
-        return (
-          <div key={id}>
-            <h1>{name}</h1>
-          </div>
-        )
+    <div className="products-container">
+      {products.map((product) => {
+        return <ProductCard key={product.id} product={product} />
       })}
-    </>
+    </div>
   )
 }
 
