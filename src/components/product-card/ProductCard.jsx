6f04fef08dac2797a873a3760c61d5fa6fa68b3a
Jake Pan <jiaxing1222@gmail.com>
2022-07-30 17:20:15 +0800

    added context function to component

--- src/components/product-card/ProductCard.jsx
+++ src/components/product-card/ProductCard.jsx
@@ -1,8 +1,17 @@
 import Button from "../button/Button"
 import "./product-card.styles.scss"
+import { CartContext } from "../../contexts/CartContext"
+
+import { useContext } from "react"
 
 const ProductCard = ({ product }) => {
   const { name, imageUrl, price } = product
+
+  const { addItemToCart } = useContext(CartContext)
+
+  const handleButtonClick = () => {
+    addItemToCart(product)
+  }
 
   return (
     <div className="product-card-container">
@@ -11,7 +20,9 @@ const ProductCard = ({ product }) => {
         <span className="name">{name}</span>
         <span className="price">{price}</span>
       </div>
-      <Button buttonType={"inverted"}>ADD TO CART</Button>
+      <Button buttonType={"inverted"} onClick={handleButtonClick}>
+        ADD TO CART
+      </Button>
     </div>
   )
 }
