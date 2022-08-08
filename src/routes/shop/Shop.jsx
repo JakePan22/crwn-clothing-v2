ef8944e91661a5105856cb45e13fdb71d4cc7c5b
Jake Pan <jiaxing1222@gmail.com>
2022-08-08 19:38:27 +0800

    changed how to handle data structure to add preview

--- src/routes/shop/Shop.jsx
+++ src/routes/shop/Shop.jsx
@@ -3,24 +3,24 @@ import { CategoriesContext } from "../../contexts/CategoriesContext"
 import ProductCard from "../../components/product-card/ProductCard"
 
 import "./shop-styles.scss"
+import CategoryPreview from "../../components/category-preview/CategoryPreview"
 
 const Shop = () => {
   const { categoriesMap } = useContext(CategoriesContext)
   return (
-    <>
+    <div className="shop-container">
       {Object.keys(categoriesMap).map((title) => {
+        const products = categoriesMap[title]
+
         return (
-          <Fragment key={title}>
-            <h2>{title}</h2>
-            <div className="products-container">
-              {categoriesMap[title].map((product) => {
-                return <ProductCard key={product.id} product={product} />
-              })}
-            </div>
-          </Fragment>
+          <CategoryPreview
+            key={title}
+            title={title}
+            products={products}
+          ></CategoryPreview>
         )
       })}
-    </>
+    </div>
   )
 }
 
