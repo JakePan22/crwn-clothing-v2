c276896cfb596635100983628b6c43366bf5b35c
Jake Pan <jiaxing1222@gmail.com>
2022-08-08 19:17:03 +0800

    adding in new mapping to cater for categoriesMap

--- src/routes/shop/Shop.jsx
+++ src/routes/shop/Shop.jsx
@@ -1,18 +1,26 @@
-import { useContext } from "react"
-import { ProductsContext } from "../../contexts/ProductsContext"
+import { useContext, Fragment } from "react"
+import { CategoriesContext } from "../../contexts/CategoriesContext"
 import ProductCard from "../../components/product-card/ProductCard"
 
 import "./shop-styles.scss"
 
 const Shop = () => {
-  const { products } = useContext(ProductsContext)
-
+  const { categoriesMap } = useContext(CategoriesContext)
   return (
-    <div className="products-container">
-      {products.map((product) => {
-        return <ProductCard key={product.id} product={product} />
+    <>
+      {Object.keys(categoriesMap).map((title) => {
+        return (
+          <Fragment key={title}>
+            <h2>{title}</h2>
+            <div className="products-container">
+              {categoriesMap[title].map((product) => {
+                return <ProductCard key={product.id} product={product} />
+              })}
+            </div>
+          </Fragment>
+        )
       })}
-    </div>
+    </>
   )
 }
 
