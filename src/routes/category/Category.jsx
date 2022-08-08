18d062b91432122cf13ea45525c80b2f349ed32e
Jake Pan <jiaxing1222@gmail.com>
2022-08-09 00:35:53 +0800

    restyled to new html structure

--- src/routes/category/Category.jsx
+++ src/routes/category/Category.jsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from "react"
+import { useContext, useEffect, useState, Fragment } from "react"
 import { useParams } from "react-router-dom"
 import ProductCard from "../../components/product-card/ProductCard"
 import { CategoriesContext } from "../../contexts/CategoriesContext"
@@ -8,19 +8,22 @@ import "./category-styles.scss"
 const Category = () => {
   const { category } = useParams()
   const { categoriesMap } = useContext(CategoriesContext)
-  const [products, setProducts] = useState(categoriesMap)
+  const [products, setProducts] = useState([])
 
   useEffect(() => {
     setProducts(categoriesMap[category])
   }, [categoriesMap, category])
 
   return (
-    <div className="category-container">
-      {products &&
-        products.map((product) => {
-          return <ProductCard key={product.id} product={product} />
-        })}
-    </div>
+    <Fragment>
+      <h2 className="title">{category.toUpperCase()}</h2>
+      <div className="category-container">
+        {products &&
+          products.map((product) => {
+            return <ProductCard key={product.id} product={product} />
+          })}
+      </div>
+    </Fragment>
   )
 }
 
