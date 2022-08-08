c593d29d683d46c70add90d3fa55a8ee6d36f1cf
Jake Pan <jiaxing1222@gmail.com>
2022-08-09 00:36:00 +0800

    added link routing for category

--- src/components/category-preview/CategoryPreview.jsx
+++ src/components/category-preview/CategoryPreview.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom"
 import ProductCard from "../product-card/ProductCard"
 
 import "./category-preview.styles.scss"
@@ -8,7 +9,9 @@ const CategoryPreview = ({ title, products }) => {
     <>
       <div className="category-preview-container">
         <h2>
-          <span className="title">{title.toUpperCase()}</span>
+          <Link className="title" to={title}>
+            {title.toUpperCase()}
+          </Link>
         </h2>
         <div className="preview">
           {filteredProducts.map((product) => {
