181c807d4c54233518162d018cea7412d21d07b4
Jake Pan <jiaxing1222@gmail.com>
2022-08-09 02:49:13 +0800

    changed to styled components. Learnt to nest the h2 and p in body and also to structure when to declare exports

--- src/components/directory-item/DirectoryItem.jsx
+++ src/components/directory-item/DirectoryItem.jsx
@@ -1,20 +1,19 @@
-import "./directory-item.styles.scss"
+import {
+  DirectoryItemContainer,
+  BackgroundImage,
+  Body,
+} from "./directory-item.styles"
 
 const DirectoryItem = ({ category }) => {
   const { title, id, imageUrl } = category
   return (
-    <div className="directory-item-container" key={id}>
-      <div
-        className="background-image"
-        style={{
-          backgroundImage: `url(${imageUrl})`,
-        }}
-      />
-      <div className="body">
+    <DirectoryItemContainer key={id}>
+      <BackgroundImage imageUrl={imageUrl} />
+      <Body>
         <h2>{title}</h2>
         <p>Shop Now</p>
-      </div>
-    </div>
+      </Body>
+    </DirectoryItemContainer>
   )
 }
 
